18919810ab45770fdc550f8d3b9091cd290112bb
Cadrach <cadrach@gmail.com>
2013-05-30 17:47:59 +0200

    Initial commit

---
+++ module/Team/config/module.config.php
@@ -0,0 +1,48 @@
+<?php
+namespace Team;
+
+return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Team\Controller\Team' => 'Team\Controller\TeamController',
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'team' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/team[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Team\Controller\Team',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'team' => __DIR__ . '/../view',
+        ),
+    ),
+    // Doctrine config
+    'doctrine' => array(
+        'driver' => array(
+            __NAMESPACE__ . '_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                )
+            )
+        )
+    )
+);
---
+++ module/Team/src/Team/Controller/TeamController.php
@@ -0,0 +1,24 @@
+<?php
+namespace Team\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class TeamController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
